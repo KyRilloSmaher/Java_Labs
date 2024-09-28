64aa77be96705c2fd6ea2e12f53c72e68e6be002
kyrillos7 <156196824+KyRilloSmaher@users.noreply.github.com>
2024-09-28 22:28:02 +0300

    lab 1 Solutions

---
+++ Lab1/Src/Main.java
@@ -0,0 +1,18 @@
+package New_labs.Lab1.Src;
+import  New_labs.Lab1.Src.Models.*;
+
+/**
+ * Main
+ */
+public class Main {
+  public static void main(String args []){
+        // QuestionOne.Dispaly();
+        //QuestionTwo.Dispaly(args);
+        //QuestionThree.Display();
+        //QuestionFour.Display();
+        //QuestionFive.Dispaly();
+        // QuestionSix.Display();
+        //QuestionSeven.Display();
+  }
+}
+  
